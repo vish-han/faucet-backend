3126b3f5a1f16e1db61dda8197949f2e292ab700
vish-han <vshlchauhab@gmail.com>
2024-07-30 12:28:43 +0530

    Rate limiting added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Web3 } = require('web3');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const FaucetAddress = '0xB50bC4F2a213e0DEa9D7B44bCBb5acF921c8A1a1';
 const PrivateKey = '9c8eb6b23de894cd1e01f8ef2718d6ea2f3d7dfa9393719c64194e04d800f22c'
@@ -8,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 
 const web3 = new Web3('https://testnet-rpc.layeredge.io');
+
+// Set up rate limiter: maximum of 2 requests per IP within a 24-hour period
+const limiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+    max: 2, // limit each IP to 2 requests per windowMs
+    message: 'You have exceeded the 2 requests in 24 hrs limit!', 
+    headers: true,
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -19,7 +31,7 @@ app.post('/', async (req, res) => {
         
         // Check balance of FaucetAddress to ensure sufficient funds
         const balance = await web3.eth.getBalance(FaucetAddress);
-        console.log(balance);
+         
         const gasLimit = 21000000; // Typical gas limit for a simple ETH transfer
         const gasPrice = web3.utils.toWei('20', 'gwei');
         const value = web3.utils.toWei('0.1', 'ether');
@@ -52,6 +64,7 @@ app.post('/', async (req, res) => {
         });
     }
 });
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
